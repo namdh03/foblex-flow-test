89b48b267700ed8d096da7e7d19d0fef5b18e53f
namdh03 <duonghoangnam503@gmail.com>
2024-11-12 04:31:33 +0000

    chore: config eslint, prettier

---
+++ eslint.config.js
@@ -0,0 +1,69 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
+const eslintPluginSimpleImportSort = require('eslint-plugin-simple-import-sort');
+
+module.exports = tseslint.config(
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+      eslintPluginPrettierRecommended,
+    ],
+    processor: angular.processInlineTemplates,
+    plugins: {
+      'simple-import-sort': eslintPluginSimpleImportSort,
+    },
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+      'prettier/prettier': [
+        'warn',
+        {
+          arrowParens: 'always',
+          bracketSameLine: false,
+          bracketSpacing: true,
+          embeddedLanguageFormatting: 'auto',
+          htmlWhitespaceSensitivity: 'css',
+          insertPragma: false,
+          singleQuote: true,
+          jsxSingleQuote: true,
+          printWidth: 120,
+          proseWrap: 'preserve',
+          quoteProps: 'as-needed',
+          requirePragma: false,
+          semi: true,
+          tabWidth: 2,
+          useTabs: false,
+          endOfLine: 'auto',
+        },
+      ],
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
+    rules: {},
+  },
+);
